c209299cbac66a78db9dc1b67deb139b31886a6b
Mark Hillard <markhillard@gmail.com>
2025-05-25 21:59:52 -0400

    Add health check

--- server.js
+++ server.js
@@ -1,41 +1,56 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const app = express();
 app.use(express.json());
 
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
 const secretKey = process.env.TURNSTILE_SECRET;
 
-app.post('/', async (req, res) => {
+app.post("/", async (req, res) => {
   const { token } = req.body;
-
+  
   if (!token) {
-    return res.status(400).json({ success: false, message: 'Token is required' });
+    return res
+      .status(400)
+      .json({ success: false, message: "Token is required" });
   }
-
+  
   try {
     const response = await axios.post(
-      'https://challenges.cloudflare.com/turnstile/v0/siteverify',
+      "https://challenges.cloudflare.com/turnstile/v0/siteverify",
       new URLSearchParams({
         secret: secretKey,
         response: token,
       }),
       {
         headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        }
-      }
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      },
     );
-
+    
     const { success } = response.data;
-
+    
     if (success) {
-      res.status(200).json({ success: true, message: 'Turnstile verification successful' });
+      res.status(200).json({
+        success: true,
+        message: "Turnstile verification successful",
+      });
     } else {
-      res.status(400).json({ success: false, message: 'Turnstile verification failed' });
+      res.status(400).json({
+        success: false,
+        message: "Turnstile verification failed",
+      });
     }
   } catch (error) {
-    console.error('Error verifying Turnstile:', error);
-    res.status(500).json({ success: false, message: 'Internal server error' });
+    console.error("Error verifying Turnstile:", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
   }
 });
 
